bf6be219f9edf2e2f22fcea1e6cbbe4de5c8afc9
PrestonPhro <prestonfrom@gmail.com>
2016-03-06 11:49:48 +0900

    Module name?

--- models/trainingModule.js
+++ models/trainingModule.js
@@ -18,11 +18,12 @@ function getTrainingModulesListing(callback) {
 }
 
 function getTrainingModule(moduleID, callback) {
-	pool.query('SELECT module.name, video.name, reading.name, quiz.name FROM moduleContent LEFT JOIN module ON moduleContent.idModule = module.id LEFT JOIN video ON moduleContent.idVideo = video.id LEFT JOIN reading ON moduleContent.idReading = reading.id LEFT JOIN quiz ON moduleContent.idQuiz = quiz.id where moduleContent.idModule = ?',
+	pool.query('SELECT module.name FROM moduleContent LEFT JOIN module ON moduleContent.idModule = module.id where moduleContent.idModule = ?',
+	//pool.query('SELECT module.name, video.name, reading.name, quiz.name FROM moduleContent LEFT JOIN module ON moduleContent.idModule = module.id LEFT JOIN video ON moduleContent.idVideo = video.id LEFT JOIN reading ON moduleContent.idReading = reading.id LEFT JOIN quiz ON moduleContent.idQuiz = quiz.id where moduleContent.idModule = ?',
 	[moduleID], function(err, rows, fields){
 	//pool.query('SELECT * FROM module WHERE id = ?', [moduleID], function (err, rows, fields) {
         console.log([moduleID]);
-		console.log(rows[0][0] + " " + rows[0][1]);
+		console.log(rows[0]);
 		if (err) {
             callback(err, null);
         } else if (rows.length === 0) {
