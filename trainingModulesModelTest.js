edfa90d1dd4826127dd788786b2da310f30dca5b
PrestonPhro <prestonfrom@gmail.com>
2016-03-07 10:55:13 +0900

    Update Testing Var Names

--- trainingModulesModelTest.js
+++ trainingModulesModelTest.js
@@ -10,7 +10,7 @@ var moduleID = 1;
 //trainingModule.getTrainingModule(moduleID,function(err,res){console.log(JSON.stringify(res))});
 
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"Guide to Defining Your Unique Selling Points by CareerOne","videoPath":"https://www.youtube.com/watch?v=FhfqOwq0Q34&feature=youtu.be","readingName":"10 Actionable Steps to Discover Your Unique Skills by Unsettle","readingPath":"http://unsettle.org/skills/","quizName":"Define Your Skills Quiz","quizID":1,"moduleName":"Module 1","moduleDescription":"Learn how to discover your unique skills.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"VideoName":"Guide to Defining Your Unique Selling Points by CareerOne","VideoPath":"https://www.youtube.com/watch?v=FhfqOwq0Q34&feature=youtu.be","ReadingName":"10 Actionable Steps to Discover Your Unique Skills by Unsettle","ReadingPath":"http://unsettle.org/skills/","QuizName":"Define Your Skills Quiz","QuizId":1,"moduleName":"Module 1","moduleDescription":"Learn how to discover your unique skills.","addedDate":"2016-02-28T00:00:00.000Z"}
     
 
     for(var prop in res)
@@ -30,7 +30,7 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 2;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"Answering the Job Ad by CareerOne","videoPath":"https://www.youtube.com/watch?v=HHr67OD9zD8","readingName":"NULL","readingPath":"NULL","quizName":"NULL","quizID":"NULL","moduleName":"Module 2","moduleDescription":"Learn how to read a job ad and create a resume.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"VideoName":"Answering the Job Ad by CareerOne","VideoPath":"https://www.youtube.com/watch?v=HHr67OD9zD8","ReadingName":"NULL","ReadingPath":"NULL","QuizName":"NULL","QuizId":"NULL","moduleName":"Module 2","moduleDescription":"Learn how to read a job ad and create a resume.","addedDate":"2016-02-28T00:00:00.000Z"}
     
     var goodResult = 1;
     for(var prop in res)
@@ -50,7 +50,7 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 3;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"Preparing for Your Interview by CareerOne","videoPath":"https://www.youtube.com/watch?v=je_XMHCnyUc","readingName":"NULL","readingPath":"NULL","quizName":"NULL","quizID":"NULL","moduleName":"Module 3","moduleDescription":"Learn how to prepare for a job interview.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"VideoName":"Preparing for Your Interview by CareerOne","VideoPath":"https://www.youtube.com/watch?v=je_XMHCnyUc","ReadingName":"NULL","ReadingPath":"NULL","QuizName":"NULL","QuizId":"NULL","moduleName":"Module 3","moduleDescription":"Learn how to prepare for a job interview.","addedDate":"2016-02-28T00:00:00.000Z"}
     
     var goodResult = 1;
     for(var prop in res)
@@ -70,7 +70,7 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 4;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"How to Present Yourself by CareerOne","videoPath":"https://www.youtube.com/watch?v=vLZ-737hGYk&feature=youtu.be","readingName":"NULL","readingPath":"NULL","quizName":"NULL","quizID":"NULL","moduleName":"Module 4","moduleDescription":"Learn how to present yourself in a job interview.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"VideoName":"How to Present Yourself by CareerOne","VideoPath":"https://www.youtube.com/watch?v=vLZ-737hGYk&feature=youtu.be","ReadingName":"NULL","ReadingPath":"NULL","QuizName":"NULL","QuizId":"NULL","moduleName":"Module 4","moduleDescription":"Learn how to present yourself in a job interview.","addedDate":"2016-02-28T00:00:00.000Z"}
     
     var goodResult = 1;
     for(var prop in res)
@@ -90,7 +90,7 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 5;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"Practice Interview Questions by CareerOne","videoPath":"https://www.youtube.com/watch?v=jNSMPrvEWxo&feature=youtu.be","readingName":"NULL","readingPath":"NULL","quizName":"NULL","quizID":"NULL","moduleName":"Module 5","moduleDescription":"Practice interview questions.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"VideoName":"Practice Interview Questions by CareerOne","VideoPath":"https://www.youtube.com/watch?v=jNSMPrvEWxo&feature=youtu.be","ReadingName":"NULL","ReadingPath":"NULL","QuizName":"NULL","QuizId":"NULL","moduleName":"Module 5","moduleDescription":"Practice interview questions.","addedDate":"2016-02-28T00:00:00.000Z"}
     
     var goodResult = 1;
     for(var prop in res)
