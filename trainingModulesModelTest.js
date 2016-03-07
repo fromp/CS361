0709eea197296670d7fe8c4c5aa99e622e627fcb
PrestonPhro <prestonfrom@gmail.com>
2016-03-07 11:19:42 +0900

    updated hardcoded

--- trainingModulesModelTest.js
+++ trainingModulesModelTest.js
@@ -10,7 +10,7 @@ var moduleID = 1;
 //trainingModule.getTrainingModule(moduleID,function(err,res){console.log(JSON.stringify(res))});
 
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"VideoName":"Guide to Defining Your Unique Selling Points by CareerOne","VideoPath":"https://www.youtube.com/watch?v=FhfqOwq0Q34&feature=youtu.be","ReadingName":"10 Actionable Steps to Discover Your Unique Skills by Unsettle","ReadingPath":"http://unsettle.org/skills/","QuizName":"Define Your Skills Quiz","QuizId":1,"moduleName":"Module 1","moduleDescription":"Learn how to discover your unique skills.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"videoName":"Guide to Defining Your Unique Selling Points by CareerOne","videoPath":"https://www.youtube.com/watch?v=FhfqOwq0Q34&feature=youtu.be","readingName":"10 Actionable Steps to Discover Your Unique Skills by Unsettle","readingPath":"http://unsettle.org/skills/","quizName":"Define Your Skills Quiz","quizID":1,"moduleName":"Module 1","moduleDescription":"Learn how to discover your unique skills."};
     
 
     for(var prop in res)
@@ -32,7 +32,7 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 2;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"VideoName":"Answering the Job Ad by CareerOne","VideoPath":"https://www.youtube.com/watch?v=HHr67OD9zD8","ReadingName":"NULL","ReadingPath":"NULL","QuizName":"NULL","QuizId":"NULL","moduleName":"Module 2","moduleDescription":"Learn how to read a job ad and create a resume.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"videoName":"Answering the Job Ad by CareerOne","videoPath":"https://www.youtube.com/watch?v=HHr67OD9zD8","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 2","moduleDescription":"Learn how to read a job ad and create a resume."};
     
     var goodResult = 1;
     for(var prop in res)
@@ -54,7 +54,7 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 3;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"VideoName":"Preparing for Your Interview by CareerOne","VideoPath":"https://www.youtube.com/watch?v=je_XMHCnyUc","ReadingName":"NULL","ReadingPath":"NULL","QuizName":"NULL","QuizId":"NULL","moduleName":"Module 3","moduleDescription":"Learn how to prepare for a job interview.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"videoName":"Preparing for Your Interview by CareerOne","videoPath":"https://www.youtube.com/watch?v=je_XMHCnyUc","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 3","moduleDescription":"Learn how to prepare for a job interview."};
     
     var goodResult = 1;
     for(var prop in res)
@@ -76,7 +76,7 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 4;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"VideoName":"How to Present Yourself by CareerOne","VideoPath":"https://www.youtube.com/watch?v=vLZ-737hGYk&feature=youtu.be","ReadingName":"NULL","ReadingPath":"NULL","QuizName":"NULL","QuizId":"NULL","moduleName":"Module 4","moduleDescription":"Learn how to present yourself in a job interview.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"videoName":"How to Present Yourself by CareerOne","videoPath":"https://www.youtube.com/watch?v=vLZ-737hGYk&feature=youtu.be","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 4","moduleDescription":"Learn how to present yourself in a job interview."};
     
     var goodResult = 1;
     for(var prop in res)
@@ -98,7 +98,7 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 5;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"VideoName":"Practice Interview Questions by CareerOne","VideoPath":"https://www.youtube.com/watch?v=jNSMPrvEWxo&feature=youtu.be","ReadingName":"NULL","ReadingPath":"NULL","QuizName":"NULL","QuizId":"NULL","moduleName":"Module 5","moduleDescription":"Practice interview questions.","addedDate":"2016-02-28T00:00:00.000Z"}
+    var result = {"videoName":"Practice Interview Questions by CareerOne","videoPath":"https://www.youtube.com/watch?v=jNSMPrvEWxo&feature=youtu.be","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 5","moduleDescription":"Practice interview questions."};
     
     var goodResult = 1;
     for(var prop in res)
