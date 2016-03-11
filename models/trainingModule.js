b4b50edacc05d0c173d61e852d75460c8c93a946
Travis Robinson <travisrobinson2006@gmail.com>
2016-03-11 14:53:31 -0800

    Added comments to back end code

--- models/trainingModule.js
+++ models/trainingModule.js
@@ -1,35 +1,36 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
+var mysql = require('mysql');//grab required package for mysql interaction
+var pool = mysql.createPool({//create pool to interact with mysql
 	host 	:	'localhost',
 	user 	:	'student',
 	password:	'default',
 	database:	'student'
 });
 
-
+//this function retrieves the list of training modules from module DB, and returns the name, description, added date, and id of that module to put on list page
 function getTrainingModulesListing(callback) {
     pool.query('SELECT id, name, description, addedDate FROM module', function (err, rows, fields) {
-        if (err) {
+        if (err) {//if no results produced, throw an error
             callback(err, null);
             return;
         }
         callback(null, rows);
     });
 }
-
+//this function retrieves info about desired training module from the various tables, compiles them into an object and returns object
+//accepts a module ID as input
 function getTrainingModule(moduleID, callback) {
 	pool.query('SELECT module.name as ModName, module.description as ModDesc, video.name as VideoName, video.filePath as VideoPath, reading.name as ReadingName, reading.contentPath as ReadingPath, quiz.id as QuizId, quiz.name as QuizName FROM moduleContent ' +
 	'LEFT JOIN module ON moduleContent.idModule = module.id '+
 	'LEFT JOIN video ON moduleContent.idVideo = video.id '+
 	'LEFT JOIN reading ON moduleContent.idReading = reading.id '+ 
 	'LEFT JOIN quiz ON moduleContent.idQuiz = quiz.id where moduleContent.idModule = ?',
-	[moduleID], function(err, rows, fields){
-		if(err){
+	[moduleID], function(err, rows, fields){//query that pulls from various tables to return info for each individual module. Renames values in query due to node.js overwriting table values with same name
+		if(err){//if failure return err
 			callback(err, null);
 		}
-		else if (rows.length === 0) {
+		else if (rows.length === 0) {//if nothing returned send message
 			callback({ name: 'NotFoundError', message: 'Module not found' }, null);   
-		} else {
+		} else {//fill outgoing object with desired values from table query
 			//outgoingData is an object containing data from the various databases
 			var outgoingData = {};
 			outgoingData.videoName = rows[0].VideoName;
--- trainingModulesModelTest.js
+++ trainingModulesModelTest.js
@@ -5,22 +5,22 @@ var trainingModule = require('./models/trainingModule.js');
 //testing w/ each known module cor correct values, testing w/ a bad module to ensure no values returned from DB
 //manually tested a bad quiry from DB
 
-var goodResult = 1;
-var moduleID = 1;
+var goodResult = 1;//bool value used as a flag to determine if a test passed or not, needs to be reset after each test
+var moduleID = 1;//value passed to getTrainingModule function--determines which module is retrieved, starting with module 1
 //trainingModule.getTrainingModule(moduleID,function(err,res){console.log(JSON.stringify(res))});
 
-trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"Guide to Defining Your Unique Selling Points by CareerOne","videoPath":"https://www.youtube.com/watch?v=FhfqOwq0Q34&feature=youtu.be","readingName":"10 Actionable Steps to Discover Your Unique Skills by Unsettle","readingPath":"http://unsettle.org/skills/","quizName":"Define Your Skills Quiz","quizID":1,"moduleName":"Module 1","moduleDescription":"Learn how to discover your unique skills."};
+trainingModule.getTrainingModule(moduleID,function(err,res){//retrieve module 1
+    var result = {"videoName":"Guide to Defining Your Unique Selling Points by CareerOne","videoPath":"https://www.youtube.com/watch?v=FhfqOwq0Q34&feature=youtu.be","readingName":"10 Actionable Steps to Discover Your Unique Skills by Unsettle","readingPath":"http://unsettle.org/skills/","quizName":"Define Your Skills Quiz","quizID":1,"moduleName":"Module 1","moduleDescription":"Learn how to discover your unique skills."};//var result = an object that contains correct output from function--exclueds date due to formatting of date and lack of need for date value
     
 
     for(var prop in res)
     {
         if (res[prop] != result[prop])
             {
-                goodResult = 0;
-                console.log("module 1: error: " + prop);
-				console.log("Res: " + res[prop]);
-				console.log("Harcoded: " + result[prop] + "\n");
+                goodResult = 0;//if res != result, change flag to indicate tests did not pass
+                console.log("module 1: error: " + prop);//log where the error is
+				console.log("Res: " + res[prop]);//log what the output is
+				console.log("Harcoded: " + result[prop] + "\n");//log what the expected output is
             }
     }
     if (goodResult === 1)
@@ -32,17 +32,17 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 2;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"Answering the Job Ad by CareerOne","videoPath":"https://www.youtube.com/watch?v=HHr67OD9zD8","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 2","moduleDescription":"Learn how to read a job ad and create a resume."};
+    var result = {"videoName":"Answering the Job Ad by CareerOne","videoPath":"https://www.youtube.com/watch?v=HHr67OD9zD8","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 2","moduleDescription":"Learn how to read a job ad and create a resume."};//var result = an object that contains correct output from function--exclueds date due to formatting of date and lack of need for date value
     
     var goodResult = 1;
     for(var prop in res)
     {
         if (res[prop] != result[prop])
             {
-                goodResult = 0;
-                console.log("module 1: error: " + prop);
-				console.log("Res: " + res[prop]);
-				console.log("Harcoded: " + result[prop] + "\n");
+                goodResult = 0;//if res != result, change flag to indicate tests did not pass
+                console.log("module 1: error: " + prop);//log where the error is
+				console.log("Res: " + res[prop]);//log what the output is
+				console.log("Harcoded: " + result[prop] + "\n");//log what the expected output is
             }
     }
     if (goodResult === 1)
@@ -54,17 +54,17 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 3;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"Preparing for Your Interview by CareerOne","videoPath":"https://www.youtube.com/watch?v=je_XMHCnyUc","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 3","moduleDescription":"Learn how to prepare for a job interview."};
+    var result = {"videoName":"Preparing for Your Interview by CareerOne","videoPath":"https://www.youtube.com/watch?v=je_XMHCnyUc","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 3","moduleDescription":"Learn how to prepare for a job interview."};//var result = an object that contains correct output from function--exclueds date due to formatting of date and lack of need for date value
     
     var goodResult = 1;
     for(var prop in res)
     {
         if (res[prop] != result[prop])
             {
-                goodResult = 0;
-				console.log("module 1: error: " + prop);
-				console.log("Res: " + res[prop]);
-				console.log("Harcoded: " + result[prop] + "\n");
+                goodResult = 0;//if res != result, change flag to indicate tests did not pass
+				console.log("module 1: error: " + prop);//log where the error is
+				console.log("Res: " + res[prop]);//log what the output is
+				console.log("Harcoded: " + result[prop] + "\n");//log what the expected output is
             }
     }
     if (goodResult === 1)
@@ -76,17 +76,17 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 4;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"How to Present Yourself by CareerOne","videoPath":"https://www.youtube.com/watch?v=vLZ-737hGYk&feature=youtu.be","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 4","moduleDescription":"Learn how to present yourself in a job interview."};
+    var result = {"videoName":"How to Present Yourself by CareerOne","videoPath":"https://www.youtube.com/watch?v=vLZ-737hGYk&feature=youtu.be","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 4","moduleDescription":"Learn how to present yourself in a job interview."};//var result = an object that contains correct output from function--exclueds date due to formatting of date and lack of need for date value
     
     var goodResult = 1;
     for(var prop in res)
     {
         if (res[prop] != result[prop])
             {
-                goodResult = 0;
-				console.log("module 1: error: " + prop);
-				console.log("Res: " + res[prop]);
-				console.log("Harcoded: " + result[prop] + "\n");
+                goodResult = 0;//if res != result, change flag to indicate tests did not pass
+				console.log("module 1: error: " + prop);//log where the error is
+				console.log("Res: " + res[prop]);//log what the output is
+				console.log("Harcoded: " + result[prop] + "\n");//log what the expected output is
             }
     }
     if (goodResult === 1)
@@ -98,17 +98,17 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 goodResult = 1;
 moduleID = 5;
 trainingModule.getTrainingModule(moduleID,function(err,res){
-    var result = {"videoName":"Practice Interview Questions by CareerOne","videoPath":"https://www.youtube.com/watch?v=jNSMPrvEWxo&feature=youtu.be","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 5","moduleDescription":"Practice interview questions."};
+    var result = {"videoName":"Practice Interview Questions by CareerOne","videoPath":"https://www.youtube.com/watch?v=jNSMPrvEWxo&feature=youtu.be","readingName":null,"readingPath":null,"quizName":null,"quizID":null,"moduleName":"Module 5","moduleDescription":"Practice interview questions."};//var result = an object that contains correct output from function--exclueds date due to formatting of date and lack of need for date value
     
     var goodResult = 1;
     for(var prop in res)
     {
         if (res[prop] != result[prop])
             {
-                goodResult = 0;
-                console.log("module 1: error: " + prop);
-				console.log("Res: " + res[prop]);
-				console.log("Harcoded: " + result[prop] + "\n");
+                goodResult = 0;//if res != result, change flag to indicate tests did not pass
+                console.log("module 1: error: " + prop);//log where the error is
+				console.log("Res: " + res[prop]);//log what the output is
+				console.log("Harcoded: " + result[prop] + "\n");//log what the expected output is
             }
     }
     if (goodResult === 1)
@@ -118,40 +118,41 @@ trainingModule.getTrainingModule(moduleID,function(err,res){
 })
 
 goodResult = 1;
-moduleID = 6;
+moduleID = 6;//test module 6: module 6 does not exist, so we expect an error
 trainingModule.getTrainingModule(moduleID,function(err,res){
         if (err)
             {
-                console.log("module 6: good job!");
+                console.log("module 6: good job!");//error received, test is passed
             }
         else
             {
-                goodResult = 0;
-                console.log("module 6: object not broken")
+                goodResult = 0;//set failure flag
+                console.log("module 6: object not broken")//if we don't get an error, fail the test. There should be an error due to mod 6 not existing
             }
 })
 
 //testing getTrainingModulesListing to ensure returning proper values from module table
+//getTrainingModulesListing returns the list of modules along with their description and date. Date not tested here due to formatting of date and lack of need for added date
 //test w/ values in table
 //manual test w/ bad query
 goodResult = 1;
 trainingModule.getTrainingModulesListing(function(err,res){
-    result = [{"id":1,"name":"Module 1","description":"Learn how to discover your unique skills."},{"id":2,"name":"Module 2","description":"Learn how to read a job ad and create a resume."},{"id":3,"name":"Module 3","description":"Learn how to prepare for a job interview."},{"id":4,"name":"Module 4","description":"Learn how to present yourself in a job interview."},{"id":5,"name":"Module 5","description":"Practice interview questions."}]//correct/expected result without date
-for (var index = 0; index < result.length; index++)
+    result = [{"id":1,"name":"Module 1","description":"Learn how to discover your unique skills."},{"id":2,"name":"Module 2","description":"Learn how to read a job ad and create a resume."},{"id":3,"name":"Module 3","description":"Learn how to prepare for a job interview."},{"id":4,"name":"Module 4","description":"Learn how to present yourself in a job interview."},{"id":5,"name":"Module 5","description":"Practice interview questions."}]//var result = an array of objects that contain correct output from function--exclueds date due to formatting of date and lack of need for date value
+for (var index = 0; index < result.length; index++)//cycle through each object contained in array
     {
-        for (var prop in result[index])
+        for (var prop in result[index])//cycle through elements of object, testing against appropriate value
             {
                 if(res[index][prop] != result[index][prop])
                     {
-                        console.log(res[index][prop]);
-                        console.log(result[index][prop]);
-                        goodResult = 0;
-                        console.log("error in getListing at: " + prop);
+                        console.log(res[index][prop]);//print object element returned
+                        console.log(result[index][prop]);//print object element expected value
+                        goodResult = 0;//if the two objects are not equal, set failure flag
+                        console.log("error in getListing at: " + prop);//print location for failure
                     }
             }
         if(goodResult === 1)
             {
-                console.log("getTrainingModulesListing produced good result");
+                console.log("getTrainingModulesListing produced good result");//if never fail an object, log success
             }
     }
 })
